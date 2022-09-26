5e978b9224efd93047dbbb5abafe1c8c7c6761ca
Leon <leonbehrndt1@gmail.com>
2022-09-27 00:47:38 +0200

    Update js file

--- components/js/contact.js
+++ components/js/contact.js
@@ -1,75 +1,79 @@
 import React, { useState } from "react";
 import "./components/css/style.css";
-import { init } from 'emailjs-com';
+import { init } from "emailjs-com";
 const JokeAPI = require("sv443-joke-api");
-init('user_id');
+init("user_id");
 
-const Contact = React.createElement({
-  render: () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [companyName, setCompanyName] = useState("");
-    const [email, setEmail] = useState("");
-    const [accepted, setAccepted] = useState("");
-    const [emailSent, setEmailSent] = useState(false);
-  
-    return (
-      <div>
-        <div className="input-fields-flex">
-          <form action="" method="get" className="email-form">
+const Contact = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [email, setEmail] = useState("");
+  const [accepted, setAccepted] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  return (
+    <div>
+      <div className="input-fields-flex">
+        <form action="" method="get" className="email-form">
+          <input
+            type="text"
+            placeholder="First Name"
+            name="first-name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="input-field"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="last-name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            className="input-field"
+          />
+          <input
+            type="text"
+            placeholder="Company Name"
+            name="company-name"
+            value={companyName}
+            onChange={(e) => setCompanyName(e.target.value)}
+            className="input-field"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-field"
+          />
+          <div className="checkbox-container-flex">
             <input
-              type="text"
-              placeholder="First Name"
-              name="first-name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              className="input-field" />
-            <input
-              type="text"
-              placeholder="Last Name"
-              name="last-name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              className="input-field" />
-            <input
-              type="text"
-              placeholder="Company Name"
-              name="company-name"
-              value={companyName}
-              onChange={(e) => setCompanyName(e.target.value)}
-              className="input-field" />
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="input-field" />
-            <div className="checkbox-container-flex">
-              <input
-                type="checkbox"
-                checked={accepted}
-                onChange={setAccepted(e.target.value)}
-                className="checkbox" />
-              <p className="conditions-text">
-                I agree to nothing and I just want a funny <a href="">Joke</a> –
-                no data is being collected.
-              </p>
-            </div>
-            <input
-              type="submit"
-              value="Continue"
-              onClick={submit}
-              className="cta-button" />
-            <span className={emailSent ? "visible" : null}>
-              Thank you. You will receive your joke shortly via email.
-            </span>
-          </form>
-        </div><hr className="divider"></hr>
+              type="checkbox"
+              checked={accepted}
+              onChange={setAccepted(e.target.value)}
+              className="checkbox"
+            />
+            <p className="conditions-text">
+              I agree to nothing and I just want a funny <a href="">Joke</a> –
+              no data is being collected.
+            </p>
+          </div>
+          <input
+            type="submit"
+            value="Continue"
+            onClick={submit}
+            className="cta-button"
+          />
+          <span className={emailSent ? "visible" : null}>
+            Thank you. You will receive your joke shortly via email.
+          </span>
+        </form>
       </div>
-    );
- )};
-});
-
+      <hr className="divider"></hr>
+    </div>
+  );
+};
 
 const submit = () => {
   if (firstName && isValidEmail(email) && accepted) {
